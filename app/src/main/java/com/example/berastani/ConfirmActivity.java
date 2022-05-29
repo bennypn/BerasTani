1572929bb6da5ed9ee4e31bb18a293dbd7afd57e
berli <berlian@gmail.com>
2022-05-29 21:22:53 +0700

    Add TransactionActivity

--- app/src/main/java/com/example/berastani/ConfirmActivity.java
+++ app/src/main/java/com/example/berastani/ConfirmActivity.java
@@ -2,6 +2,8 @@ package com.example.berastani;
 
 import static com.example.berastani.HomepagePembeli.pera;
 import static com.example.berastani.HomepagePembeli.pulen;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,13 +12,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class ConfirmActivity extends AppCompatActivity {
 
     private TextView jenisBeras, count, total;
     private String berasPera, berasPulen;
     private Button btnBayar;
-    private ImageView add,min;
+    private ImageView add,min, back;
     protected static Integer cntPera, cntPulen, totalPrice = 0;
     private Integer cnt = 0;
 
@@ -31,6 +38,7 @@ public class ConfirmActivity extends AppCompatActivity {
         total = findViewById(R.id.total_txt);
         add = findViewById(R.id.add_item);
         min = findViewById(R.id.minus_item);
+        back = findViewById(R.id.back_btn);
 
         berasPera = "Beras Petruk Pera";
         berasPulen = "Beras Rojolele Pulen";
@@ -91,15 +99,33 @@ public class ConfirmActivity extends AppCompatActivity {
             }
         });
 
-        btnBayar.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(ConfirmActivity.this, TransactionActivity.class);
+                Intent i = new Intent(ConfirmActivity.this, HomepagePembeli.class);
                 startActivity(i);
             }
         });
 
+        btnBayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                barcodeLauncher.launch(new ScanOptions());
 
+                Intent i = new Intent(ConfirmActivity.this, TransactionActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+    // Register the launcher and result handler
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if(result.getContents() == null) {
+                    Toast.makeText(ConfirmActivity.this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(ConfirmActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            });
 }
--- app/src/main/java/com/example/berastani/TransactionActivity.java
+++ app/src/main/java/com/example/berastani/TransactionActivity.java
@@ -1,14 +1,54 @@
 package com.example.berastani;
 
+import static com.example.berastani.ConfirmActivity.cntPera;
+import static com.example.berastani.ConfirmActivity.cntPulen;
+import static com.example.berastani.ConfirmActivity.totalPrice;
+import static com.example.berastani.HomepagePembeli.pera;
+import static com.example.berastani.HomepagePembeli.pulen;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TransactionActivity extends AppCompatActivity {
+
+    private TextView total, jenis, jumlah;
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
+
+        total = findViewById(R.id.totalTxt);
+        jenis = findViewById(R.id.jenisBerastxt);
+        jumlah = findViewById(R.id.JumlahBerastxt);
+        back = findViewById(R.id.btnKeluar);
+
+        total.setText(totalPrice.toString());
+
+        if(pera){
+            jenis.setText("Beras Petruk Pera");
+            jumlah.setText(cntPera.toString());
+
+        } else if(pulen) {
+            jenis.setText("Beras Rojolele Pulen");
+            jumlah.setText(cntPulen.toString());
+
+        } else {
+            jenis.setText("Error TV");
+        }
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TransactionActivity.this, HomepagePembeli.class);
+                startActivity(i);
+            }
+        });
     }
 }
